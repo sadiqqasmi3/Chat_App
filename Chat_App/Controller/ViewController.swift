515f0a2d48b496376f247ef95e6f71d2a991ebb6
sadiq <sadiq.qasmi3@gmail.com>
2021-01-23 14:48:09 +0500

    done with ui

--- Chat_App/Controller/ViewController.swift
+++ Chat_App/Controller/ViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class ViewController: UIViewController {
 
+    @IBOutlet var titalLabel: CLTypingLabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        titalLabel.text = "Chat app"
         // Do any additional setup after loading the view.
     }
 
